429bb00a560f20f29c83a5f1fd7296d9a77b2fe5
aranega <vincent.aranega@gmail.com>
2021-08-12 17:28:24 +0200

    Add setup.py and MANIFEST.in

---
+++ setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import sys
+from setuptools import setup
+import motra
+
+packages = ['motra',
+            'motra.trace']
+
+if sys.version_info < (3, 5):
+    sys.exit('Sorry, Python < 3.5 is not supported')
+
+setup(
+    name='motra',
+    version=motra.__version__,
+    description=('A Python(ic) implementation of the UML2 metamodel'),
+    long_description=open('README.rst').read(),
+    keywords='model metamodel MDE M2M M2T traceability transformation',
+    url='https://github.com/pyecore/pyuml2',
+    author='Vincent Aranega',
+    author_email='vincent.aranega@gmail.com',
+
+    packages=packages,
+    package_data={'': ['README.rst', 'LICENSE', 'CHANGELOG.rst']},
+    include_package_data=True,
+    install_requires=required_packages,
+    tests_require=['pytest'],
+    license='BSD 3-Clause',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'License :: OSI Approved :: BSD License',
+    ]
+)
