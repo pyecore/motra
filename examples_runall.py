c0b3e8e54b46572c3bc10bb2b719102e267c371b
aranega <vincent.aranega@gmail.com>
2021-10-27 07:53:01 +0200

    Add and comment use of the chain2dot m2t

--- examples_runall.py
+++ examples_runall.py
@@ -1,6 +1,7 @@
 from examples.m2m.inplace import changename, variable_changename, duplicate, cache, polymorphic
 from examples.m2t.sample import ecore2dot, ecore2simplejava
 from examples.chain.simple_chain import ecore2javalike
+# from motra.chain import chain_printer
 
 print("* Runs in-place transformations", end="")
 result_context = changename.run(ecore_model="examples/inputs/input.ecore")
@@ -30,3 +31,5 @@ print("     [DONE]")
 print("* Runs full transformations chains", end="")
 ecore2javalike.run('examples/inputs/input.ecore')
 print("     [DONE]")
+
+# print(chain_printer.run(ecore2javalike.chain, output_path='.'))
